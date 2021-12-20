256e68e0fb1c52c7599b757790ca45fc199fc865
Vladimir Bludov <vladimir.bloodov@gmail.com>
2021-12-20 17:48:09 +0200

    second task finished

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+refs.galleryContainer.innerHTML = galleryMarkup;
+
+const galleryModalWindow = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(item => createGalleryItem(item)).join('');
+}
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        loading="lazy"
+      />
+    </a>`;
+}
